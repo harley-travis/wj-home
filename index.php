f229ad528134b137a046b1c839e49885e63da92e
harley-travis <harley.travis2@gmail.com>
2017-08-21 15:22:38 -0600

    these files

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>White July | Home</title>
+        
+        <?php require_once(dirname(__FILE__)."/config.php"); ?>
+        
+        <!-- meta -->
+		<meta charset="utf-8">
+		<meta name="description" content="White July is a software solution to help manage companies, and HR departments, find and hire the best employees. Our software allows you to create job postings, manage applicants and interviews and even manage current employees.">
+		<meta name="keywords" content="career recruitment, software solution, employee management, job hiring software">
+		<meta name="author" content="White July">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- css -->
+        <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        
+        <!-- js -->
+        <script src="<?php echo ROOT; ?>/assets/js/3.2.0/jquery-3.2.0.slim.min.js"></script>
+        
+        <!-- analytics -->
+        
+        <!-- heatmap -->
+        
+    </head>
+    <body>
+        <header class="container-fluid">
+            <div class="container header-wrapper">
+                <div class="logo">
+                    <h1 class="logo">White July</h1>
+                </div><!-- logo -->
+                <nav>
+                    <ul class="nav nav-pills">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Software</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        
+        <!-- banner -->
+        <section class="container-fluid home-banner">
+        	<div class="container banner">
+        		<h2>Career Recruitement and Employee Management Software Solutions</h2>
+        		<div class="banner-cta">
+        			<a href="#" class="btn btn-primary">Watch Video</a>
+        			<a href="#" class="btn btn-primary">Learn More</a>
+        		</div>
+        	</div>
+        </section>
+        
+        <!-- software features -->
+        <section class="container-fluid">
+        	<div class="container">
+        		
+        	</div>
+        </section>
+        
+        <!-- Video -->
+         <section class="container-fluid">
+        	<div class="container">
+        		
+        	</div>
+        </section>
+        
+        <!-- Testimony -->
+         <section class="container-fluid">
+        	<div class="container">
+        		
+        	</div>
+        </section>
+        
+        <!-- Contact From -->
+         <section class="container-fluid">
+        	<div class="container">
+        		
+        	</div>
+        </section>
+        
+        <footer class="container-fluid">
+            <div class="container footer-wrapper">
+            	<ul>
+            		<li><a href="#">Terms of Use</a></li>
+            		<li><a href="#">Privacy Policy</a></li>
+            	</ul>
+            	<span><?php echo date(Y); ?> &copy; WhiteJuly.com | All Rights Reserved.</span>
+            </div><!-- footer-wrapper -->
+        </footer>
+    </body>
+</html>
